850a1dace6ef5a2019b23e5d742c4e2a66c21bf6
Vitaly Bichov <vitalyb@wix.com>
2019-04-11 10:51:55 +0300

    balbla

--- utils/dawn/skyline.dawn.sh
+++ utils/dawn/skyline.dawn.sh
@@ -36,15 +36,16 @@
 # Please replace the values here and populate these variables as appropriate
 # with the values of YOUR set up or write them to /etc/skyline/skyline.dawn.conf
 # to be sourced.
+SHORT_HOSTNAME=$(echo "${HOSTNAME}" | cut -d"." -f1)
 YOUR_SERVER_IP_ADDRESS="$(ifconfig eth0 | grep -o "inet.*" | cut -d " " -f2)"                         # YOUR Skyline server public IP address
 YOUR_SKYLINE_SERVER_FQDN="$(hostname -f)"        # YOUR Skyline server FQDN
 YOUR_EMAIL="skyline@wix.com"                                # YOUR email address for the httpd server admin
 YOUR_OTHER_IP_ADDRESS="0.0.0.0"                            # YOUR current public IP address that you will be connecting from
 WEBAPP_AUTH_USER="admin"                                   # The username you want to use for http authentication
-WEBAPP_AUTH_USER_PASSWORD="$(echo ${HOSTNAME}_skyline)"    # The password you want to use for http authentication
-MYSQL_ROOT_PASSWORD="$(echo ${HOSTNAME}_root_skyline)"      # The MySQL root user password
-MYSQL_SKYLINE_PASSWORD="$(echo ${HOSTNAME}_user_skyline)"   # The Skyline DB user password
-REDIS_PASSWORD="$(echo ${HOSTNAME}_redis_skyline)"        # The Redis password
+WEBAPP_AUTH_USER_PASSWORD="${SHORT_HOSTNAME}_skyline"    # The password you want to use for http authentication
+MYSQL_ROOT_PASSWORD="${SHORT_HOSTNAME}_root_skyline"      # The MySQL root user password
+MYSQL_SKYLINE_PASSWORD="${SHORT_HOSTNAME}_user_skyline"   # The Skyline DB user password
+REDIS_PASSWORD="${SHORT_HOSTNAME}_redis_skyline"        # The Redis password
 SKYLINE_RELEASE="master"                 # The Skyline release to deploy
 
 STARTED=$(date)
