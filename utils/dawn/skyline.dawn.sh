8f9e118e84634fa4a38c8243c00c64f0d870049b
Vitaly Bichov <vitalyb@wix.com>
2019-04-11 09:47:10 +0300

    I'm to layzy

--- utils/dawn/skyline.dawn.sh
+++ utils/dawn/skyline.dawn.sh
@@ -44,7 +44,7 @@ WEBAPP_AUTH_USER="admin"                                   # The username you wa
 WEBAPP_AUTH_USER_PASSWORD="$(echo {$HOSTNAME}_skyline)"    # The password you want to use for http authentication
 MYSQL_ROOT_PASSWORD="$(echo {$HOSTNAME}_sql_root_skyline)"     # The MySQL root user password
 MYSQL_SKYLINE_PASSWORD="$(echo {$HOSTNAME}_sql_skyline)"  # The Skyline DB user password
-REDIS_PASSWORD=$(echo {$HOSTNAME}_skyline)"       # The Redis password
+REDIS_PASSWORD="$(echo {$HOSTNAME}_skyline)"       # The Redis password
 SKYLINE_RELEASE="v1.2.121"                 # The Skyline release to deploy
 
 STARTED=$(date)
